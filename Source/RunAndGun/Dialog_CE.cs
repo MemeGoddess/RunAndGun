f89d29d2447db1c0375aed93f0abdd108462a309
Lana Avery <memekoten@gmail.com>
2025-08-16 16:13:48 +1000

    Fixed a couple settings and did some more tidying

--- Source/RunAndGun/Dialog_CE.cs
+++ Source/RunAndGun/Dialog_CE.cs
@@ -23,10 +23,7 @@ namespace RunAndGun
         private readonly string message;
         private bool closedLogWindow;
 
-        public override Vector2 InitialSize
-        {
-            get { return new Vector2(500f, 400f); }
-        }
+        public override Vector2 InitialSize => new Vector2(500f, 400f);
 
         public Dialog_CE(string title, string message)
         {
@@ -57,11 +54,14 @@ namespace RunAndGun
                 logWindow.Close(false);
                 closedLogWindow = true;
             }
+
             Text.Font = GameFont.Medium;
             var titleRect = new Rect(inRect.x, inRect.y, inRect.width, 40);
             Widgets.Label(titleRect, title);
+
             Text.Font = GameFont.Small;
             Widgets.Label(new Rect(inRect.x, inRect.y + titleRect.height, inRect.width, inRect.height - ConfirmButtonSize.y - titleRect.height), message);
+
             Rect closeButtonRect;
             var prevColor = GUI.color;
             GUI.color = ConfirmButtonColor;
@@ -74,6 +74,7 @@ namespace RunAndGun
                 HugsLibController.SettingsManager.SaveChanges();
             }
             GUI.color = prevColor;
+
             closeButtonRect = new Rect(inRect.width - CloseButSize.x, inRect.height - CloseButSize.y, CloseButSize.x, CloseButSize.y);
             if (Widgets.ButtonText(closeButtonRect, "RG_Dialog_CE_DisallowButton".Translate()))
             {
